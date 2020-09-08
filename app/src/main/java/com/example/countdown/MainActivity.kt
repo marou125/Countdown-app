d9c6dd2d60157ba4f088be58ed094e90be0ba6c1
Matthias Marusczyk <deipst.mm@gmail.com>
2020-09-08 18:17:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/countdown/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.countdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.floatingActionButton)
+        fab.setOnClickListener{view ->
+            
+        }
+
+    }
+}
