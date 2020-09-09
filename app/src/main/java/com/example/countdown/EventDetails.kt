a653c58a7772eca732b138cf2ea7661b1b51fccc
Matthias Marusczyk <deipst.mm@gmail.com>
2020-09-09 15:11:21 +0200

    Added an Up Button to the EventDetails screen and also inserted a CalendarPicker

--- app/src/main/java/com/example/countdown/EventDetails.kt
+++ app/src/main/java/com/example/countdown/EventDetails.kt
@@ -2,10 +2,27 @@ package com.example.countdown
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.app.NavUtils
 
 class EventDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_details)
+
+        actionBar?.setDisplayHomeAsUpEnabled(true);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+
+        }
     }
 }
